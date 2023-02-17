a82aee3bdcb04e7d8c1d5014d3b4799aec74020e
jdrcabrera <jd.rcabrera@gmail.com>
2023-02-17 17:35:35 -0500

    menu corrected

--- js/time.js
+++ js/time.js
@@ -1,3 +1,6 @@
 setTimeout(function(){
 document.querySelector('.pageLoaders').classList.add('removingLoader')
 },3000)
+setTimeout(function(){
+document.querySelector('.pageLoaders').classList.add('newDisplay')
+},6000)
